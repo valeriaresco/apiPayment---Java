d1ad3ffc478c52f5048452e021fade86c4777867
valeriaresco <valeriaresco99@gmail.com>
2023-03-02 11:16:50 -0300

    Criando projeto e a classe pagamento

---
+++ src/main/java/com/javaapi/demo/PagamentoApiApplication.java
@@ -0,0 +1,14 @@
+package com.javaapi.demo;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentoApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/javaapi/demo/model/Pagamento.java
@@ -0,0 +1,99 @@
+package com.javaapi.demo.model;
+
+
+import jakarta.persistence.Entity;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+//import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Table(name = "Pagamento")
+public class Pagamento {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	//@NotNull
+	 private Integer cod_debit;
+	 
+	//@NotNull
+	private Long doc_payer;
+	
+	//@NotNull
+	private String payment_method;
+	
+	//@NotNull
+	private Float payment_amount;
+	
+	//@NotNull
+		private String status;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getCod_debit() {
+		return cod_debit;
+	}
+
+	public void setCod_debit(Integer cod_debit) {
+		this.cod_debit = cod_debit;
+	}
+
+	public Long getDoc_payer() {
+		return doc_payer;
+	}
+
+	public void setDoc_payer(Long doc_payer) {
+		this.doc_payer = doc_payer;
+	}
+
+	public String getPayment_method() {
+		return payment_method;
+	}
+
+	public void setPayment_method(String payment_method) {
+		this.payment_method = payment_method;
+	}
+
+	public Float getPayment_amount() {
+		return payment_amount;
+	}
+
+	public void setPayment_amount(Float payment_amount) {
+		this.payment_amount = payment_amount;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	
+}
